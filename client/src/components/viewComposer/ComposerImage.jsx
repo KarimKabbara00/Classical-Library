8d4fc6601925a6cd136413a354c0619c2099c798
Karim Kabbara <KarimK@geotechenv.com>
2024-08-01 09:14:43 -0600

    server only

--- client/src/components/viewComposer/ComposerImage.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "../../css/viewComposer.module.css";
-
-function ComposerImage(props) {
-  return <img alt="composer portrait" className={styles.composerImage} src={props.portrait} />;
-}
-
-export default ComposerImage;
